efdf77a91e0d26d086e9dde5277a6eb285452fad
Victor Lin <bornstub@gmail.com>
2012-02-29 21:44:29 +0800

    Add test for default domain

--- pyramid_genshi/tests/test_pyramid_genshi.py
+++ pyramid_genshi/tests/test_pyramid_genshi.py
@@ -83,7 +83,31 @@ class GenshiTemplateRendererTests(unittest.TestCase):
         self.assertEqual(result, 
                          '<div>Hola World</div>')
         
+    def test_default_domain(self):
+        from pyramid.threadlocal import get_current_registry
+        reg = get_current_registry()
+        reg.settings['genshi.default_domain'] = 'test_domain'
+        lookup = DummyLookup()
+        translate_calls = []
+        def translate(msg):
+            translate_calls.append(msg)
+            return msg
+        lookup.translate = translate
+        
+        path = self._get_template_path('i18n_msg.genshi')
+        renderer = self.make_one(path, lookup)
+        renderer({}, {})
+        self.assertEqual(len(translate_calls), 2)
+        ts1 = translate_calls[0]
+        ts2 = translate_calls[1]
+        self.assertEqual(ts1.domain, 'test_domain')
+        self.assertEqual(ts2.domain, 'test_domain')
+        
     def test_i18n_domain(self):
+        from pyramid.threadlocal import get_current_registry
+        reg = get_current_registry()
+        reg.settings['genshi.default_domain'] = 'my_domain'
+        
         from translationstring import Translator
         translated_calls = []
         lookup = DummyLookup()
