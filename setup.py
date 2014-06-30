0cc7253a7456bb6203dac5a181d7e3b4f95adc8e
Victor Lin <bornstub@gmail.com>
2014-06-29 23:26:15 -0700

    Refactory outdated stuff

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from distribute_setup import use_setuptools
+from ez_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, find_packages
@@ -14,16 +14,7 @@ except:
     CHANGES = ''
 
 
-def read_requirements(filename):
-    content = open(os.path.join(here, filename)).read()
-    requirements = map(lambda r: r.strip(), content.splitlines())
-    return requirements
-
-
-requirements = read_requirements('requirements.txt')
-test_requirements = read_requirements('test-requirements.txt')
-
-version = '0.1.3'
+version = '0.1.4'
 
 setup(
     name='pyramid_genshi',
@@ -44,6 +35,13 @@ setup(
     include_package_data=True,
     zip_safe=False,
     test_suite="pyramid_genshi.tests",
-    install_requires=requirements,
-    test_requires=test_requirements, 
+    install_requires=[
+        'Genshi',
+        'Pyramid',
+    ],
+    test_requires=[
+        'nose',
+        'nose-cov',
+        'mock',
+    ],
 )
